be90f84017ed18ef5a569696b86da427436fdbf5
Nick Meehan <nnmeehan@gmail.com>
2014-06-17 21:53:24 -0700

    Added rails skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RainbowPopcorn::Application.config.secret_token = 'e036c838cb24b54e516e04dcd0170e572ac067ba465d17d5b29b0662ba559a21307e7f1bb1fde77dc8c49781793f10d41835ed1a72c2d8ff05f34ba1d5615942'
