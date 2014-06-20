6b8650ccdf0b8824c96dde71bf5d2dcfc7c78877
Nick Meehan <nnmeehan@gmail.com>
2014-06-20 14:40:13 -0700

    Added function to determine bar position and status.

--- app/assets/javascripts/deliciousnessProgressBar.js
+++ app/assets/javascripts/deliciousnessProgressBar.js
@@ -4,5 +4,15 @@ function DeliciousnessProgressBar(colourValues) {
 }
 
 DeliciousnessProgressBar.prototype = {
-	
+	getPositionAndColour: function(addedFlavourValue) {
+    this.updateBarStatus(addedFlavourValue)
+    for (colour in this.colourValues) {
+      if (this.barStatus >= this.colourValues[colour]) {
+        return [colour, this.barStatus]
+      }
+    }
+  },
+  updateBarStatus: function(addedFlavourValue) {
+    this.barStatus += addedFlavourValue
+  }
 }
