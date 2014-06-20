2e45c623e9f2f8535bcd4408dc99ed3e3995e10e
Nick Meehan <nnmeehan@gmail.com>
2014-06-20 00:53:51 -0700

    Added values for deliciousness and progress bar.

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
-	var deliciousnessProgressBar = new DeliciousnessProgressBar()
-	new Controller(new FlavoursView(flavoursViewSelectors), new DeliciousnessView(), deliciousnessProgressBar).init()
+	var deliciousnessProgressBar = new DeliciousnessProgressBar(colourValues)
+	new Controller(new FlavoursView(flavoursViewSelectors), new DeliciousnessView(), deliciousnessProgressBar, flavourDeliciousnessValues).init()
 })
 
 var flavoursViewSelectors = {
 	flavourSelector: 'li',
 	dropZoneSelector: '.drop_zone'
+}
+
+var flavourDeliciousnessValues = {
+	ketchup: 7,
+	butter: 10,
+	truffleOil: 13,
+	dillPickle: 7,
+	blueCheese: 7,
+	salt: 4,
+	pepper: 4
+}
+
+var colourValues = {
+	violet: 90,
+	purple: 75,
+	blue: 60,
+	green: 45,
+	yellow: 30,
+	orange: 15,
+	red: 0
 }
